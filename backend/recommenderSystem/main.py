4f06bbc96258644f16adbacaa8b37b98ba9f5ce9
Wahaj Javed <84095994+WahajJaved20@users.noreply.github.com>
2024-05-06 20:35:20 +0500

    updates

--- backend/recommenderSystem/main.py
+++ backend/recommenderSystem/main.py
@@ -1,1 +1,12 @@
-#recommender system idhar
+import pandas as pd
+
+class AnimeRecommenderSystem:
+    def __init__(self):
+        pass
+    def activateFilters(self, filters):
+        pass
+    def synopsisRecommender(self):
+        pass
+
+if __name__ == "__main__":
+    print("lol")
